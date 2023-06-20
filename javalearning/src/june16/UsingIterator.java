476c3a1f3e6156a7ff5e541993ad1c6c20ec4e91
tarunvemasani <tarunvemasani454@gmail.com>
2023-06-20 12:16:35 -0500

    array list

--- javalearning/src/june16/UsingIterator.java
+++ javalearning/src/june16/UsingIterator.java
@@ -1,15 +1,17 @@
 package june16;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.*;
 
-public class Iterator<S> {
+public class UsingIterator {
     public static void main(String[] args){
         ArrayList<String> myNumbers = new ArrayList<String>();
         myNumbers.add("del");
         myNumbers.add("mum");
         myNumbers.add("bom");
         myNumbers.add("tex");
-        java.util.Iterator<String> it =myNumbers.iterator();
+        Iterator it =myNumbers.iterator();
         while(it.hasNext())
         {
             System.out.println(it.next());
